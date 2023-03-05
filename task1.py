7fc87062fe3b6a75a155dc648a1d0160d913eff9
Alla_Dseshulska <dzeshulska@gmail.com>
2023-03-05 19:16:29 +0200

    change name of file

---
+++ task1.py
@@ -0,0 +1,12 @@
+store = [
+    {
+    "label" : "kit-kat",
+    "price" : "200"
+    },
+    {"label" : "nuts",
+    "price" : "300"
+    },
+    {"label" : "coca-cola",
+    "price" : "350"
+    }
+]
